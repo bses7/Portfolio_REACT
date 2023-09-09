746e8cac959109faa2fde49411253f0d632d06cc
Bishesh <bishesh.giri8848@gmail.com>
2023-09-09 21:15:01 +0545

    Added Navbar and navmobile for responsiveness

---
+++ src/components/Darkmode.jsx
@@ -0,0 +1,50 @@
+import { useEffect, useState } from "react";
+// import { ReactComponent as Sun } from "../images/Sun.svg";
+// import { ReactComponent as Moon } from "../images/Moon.svg";
+import { ReactComponent as Sun } from "../images/Sun.svg";
+import { ReactComponent as Moon } from "../images/Moon.svg";
+
+const Darkmode = () => {
+  const [theme, setTheme] = useState("light-theme");
+  const toggleTheme = () => {
+    if (theme === "dark-theme") {
+      setTheme("light-theme");
+    } else {
+      setTheme("dark-theme");
+    }
+  };
+
+  useEffect(() => {
+    document.body.className = theme;
+  }, [theme]);
+
+  return (
+    <section className="lg: flex items-center lg:bg-cover lg:bg-center lg:bg-no-repeat py-32 lg:py-0 overflow-hidden">
+      <a
+        href="#"
+        className={`btn relative bg-[#ebe6ff] text-white transition-colors duration-300 ease-in-out`}
+        onClick={() => toggleTheme()}
+      >
+        <div
+          className={`slider w-14 h-8 rounded-full overflow-hidden relative transition-transform duration-300 ease-in-out ${
+            theme === "dark-theme" ? "bg-[#7ac8c1]" : "bg-[#7ac8c1]"
+          }`}
+        >
+          <div
+            className={`slider-icon absolute w-8 h-8 ${
+              theme === "dark-theme" ? "right-0" : "left-0"
+            } top-0 bottom-0 m-auto bg-white rounded-full shadow-md transition-transform duration-300 ease-in-out`}
+          >
+            {theme === "dark-theme" ? (
+              <Sun className="text-gray-400 " />
+            ) : (
+              <Moon className="text-gray-400 " />
+            )}
+          </div>
+        </div>
+      </a>
+    </section>
+  );
+};
+
+export default Darkmode;
---
+++ src/components/Navmobile.jsx
@@ -0,0 +1,84 @@
+import { useState } from "react";
+import { Bars3Icon } from "@heroicons/react/24/outline";
+import { XMarkIcon } from "@heroicons/react/24/outline";
+import Social from "./social";
+
+import { motion } from "framer-motion";
+import { Link } from "react-scroll";
+import { links } from "../data";
+
+const Navmobile = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const circleVariants = {
+    hidden: {
+      scale: 0,
+    },
+    visible: {
+      scale: 180,
+      transition: {
+        type: "spring",
+        stiffness: 160,
+        damping: 60,
+      },
+    },
+  };
+
+  const ulVariants = {
+    hidden: {
+      opacity: 0,
+    },
+    visible: {
+      opacity: 1,
+      transition: {
+        delay: 0.1,
+      },
+    },
+  };
+  return (
+    <nav className="relative">
+      <div onClick={() => setIsOpen(true)} className="cursor-pointer">
+        <Bars3Icon className="w-10 h-10" />
+      </div>
+      <motion.div
+        variants={circleVariants}
+        initial="hidden"
+        animate={isOpen ? "visible" : "hidden"}
+        className="w-4 h-4 rounded-full bg-accent fixed top-0 right-0"
+      ></motion.div>
+
+      <motion.ul
+        variants={ulVariants}
+        initial="hidden"
+        animate={isOpen ? "visible" : ""}
+        className={`${
+          isOpen ? "right-0" : "-right-full"
+        } fixed top-0 bottom-0 w-full flex flex-col justify-center items-center transition-all duration-300 overflow-hidden`}
+      >
+        <div
+          className="cursor-pointer absolute top-8 right-8"
+          onClick={() => setIsOpen(false)}
+        >
+          <XMarkIcon className="w-8 h-8" />
+        </div>
+        {links.map((item, index) => {
+          return (
+            <li key={index} className="mb-8">
+              <Link
+                to={item.href}
+                smooth={true}
+                duration={500}
+                offset={-70}
+                className="text-xl cursor-pointer capitalize"
+              >
+                {item.label}
+              </Link>
+            </li>
+          );
+        })}
+      </motion.ul>
+    </nav>
+  );
+};
+
+export default Navmobile;
