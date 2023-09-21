0151b703eaf59aaca7e21f3ca2df809c53a5704b
Bishesh <bishesh.giri8848@gmail.com>
2023-09-21 13:31:37 +0545

    Added new arrays (portfolio section) to the data file

--- src/data.jsx
+++ src/data.jsx
@@ -163,3 +163,57 @@ export const Services_Card = [
     ),
   },
 ];
+
+export const projectsData = [
+  {
+    id: "1",
+    image: Project1,
+    name: "project name 1",
+    category: "UI/UX design",
+  },
+  {
+    id: "2",
+    image: Project2,
+    name: "project name 2",
+    category: "web development",
+  },
+  {
+    id: "3",
+    image: Project3,
+    name: "project name 3",
+    category: "UI/UX design",
+  },
+  {
+    id: "4",
+    image: Project4,
+    name: "project name 4",
+    category: "branding",
+  },
+  {
+    id: "5",
+    image: Project5,
+    name: "project name 5",
+    category: "web development",
+  },
+  {
+    id: "6",
+    image: Project6,
+    name: "project name 6",
+    category: "web development",
+  },
+];
+
+export const projectsNav = [
+  {
+    name: "all",
+  },
+  {
+    name: "UI/UX Design",
+  },
+  {
+    name: "web development",
+  },
+  {
+    name: "branding",
+  },
+];
